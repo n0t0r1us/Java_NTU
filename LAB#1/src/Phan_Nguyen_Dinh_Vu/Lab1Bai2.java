db10d919e3178731e867a0907b06f905e5b2e357
n0t0r1us <conganolaca2869@gmail.com>
2021-09-28 18:19:20 +0700

    Java Exercise

---
+++ LAB#1/src/Phan_Nguyen_Dinh_Vu/Lab1Bai2.java
@@ -0,0 +1,26 @@
+package Phan_Nguyen_Dinh_Vu;
+
+import java.util.Scanner;
+import java.math.*;
+public class Lab1Bai2 {
+	
+	public static void main(String[] args) {
+		
+	Scanner input = new Scanner(System.in);
+        System.out.println("Nhap chieu dai HCN: ");
+	int chieuDai = input.nextInt();
+	System.out.println("Nhap chieu rong HCN: ");
+	int chieuRong = input.nextInt();
+	
+	int chuVi = (chieuDai + chieuRong) * 2;
+	
+	int dienTich = chieuDai * chieuRong;
+	
+	int min = Math.min(chieuDai, chieuRong);
+
+        System.out.printf("Chu vi HCN: %d \n ", chuVi);
+	System.out.printf("Dien tich HCN: %d \n ", dienTich);
+	System.out.printf("Canh nho nhat cua HCN: %d", min);
+    }
+}
+	
---
+++ LAB#1/src/Phan_Nguyen_Dinh_Vu/Lab1Bai3.java
@@ -0,0 +1,13 @@
+package Phan_Nguyen_Dinh_Vu;
+
+import java.util.Scanner;
+public class Lab1Bai3 {
+	
+	public static void main(String[] args) {
+        System.out.println("Nhap vao do dai cua canh: ");
+        Scanner input = new Scanner(System.in);
+	double edge = input.nextDouble();
+
+	System.out.println("The tich cua hinh la: " + Math.pow(edge, 3));
+   }	
+}
