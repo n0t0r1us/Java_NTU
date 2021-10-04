6b026ded292287754ab6b7ef059eb9b8ab9c8bc9
n0t0r1us <conganolaca2869@gmail.com>
2021-10-04 20:05:25 +0700

    Java Exercise

--- GUI/src/Phan_Nguyen_Dinh_Vu/BMISwing/BMISwing.java
+++ GUI/src/Phan_Nguyen_Dinh_Vu/BMISwing/BMISwing.java
@@ -10,8 +10,8 @@ import javax.swing.border.TitledBorder;
 public class BMISwing {
 
 	private JFrame frmChngTrnhTnh;
-	private JTextField txtCao;
-	private JTextField txtNang;
+	private JTextField txtCC;
+	private JTextField txtCN;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
@@ -42,6 +42,7 @@ public class BMISwing {
 	 */
 	private void initialize() {
 		frmChngTrnhTnh = new JFrame();
+		frmChngTrnhTnh.getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		frmChngTrnhTnh.setFont(new Font("Times New Roman", Font.PLAIN, 14));
 		frmChngTrnhTnh.setTitle("Chương trình tính chỉ số BMI");
 		frmChngTrnhTnh.setBounds(100, 100, 678, 339);
@@ -54,10 +55,10 @@ public class BMISwing {
 		lblNhapCao.setBounds(18, 62, 139, 25);
 		frmChngTrnhTnh.getContentPane().add(lblNhapCao);
 		
-		txtCao = new JTextField();
-		txtCao.setBounds(152, 64, 68, 19);
-		frmChngTrnhTnh.getContentPane().add(txtCao);
-		txtCao.setColumns(10);
+		txtCC = new JTextField();
+		txtCC.setBounds(152, 64, 68, 19);
+		frmChngTrnhTnh.getContentPane().add(txtCC);
+		txtCC.setColumns(10);
 		
 		JLabel lblNhapNang = new JLabel("Cân nặng (kg) : ");
 		lblNhapNang.setFont(new Font("Times New Roman", Font.BOLD, 13));
@@ -65,10 +66,10 @@ public class BMISwing {
 		lblNhapNang.setBounds(20, 97, 139, 25);
 		frmChngTrnhTnh.getContentPane().add(lblNhapNang);
 		
-		txtNang = new JTextField();
-		txtNang.setBounds(152, 99, 68, 19);
-		frmChngTrnhTnh.getContentPane().add(txtNang);
-		txtNang.setColumns(10);
+		txtCN = new JTextField();
+		txtCN.setBounds(152, 99, 68, 19);
+		frmChngTrnhTnh.getContentPane().add(txtCN);
+		txtCN.setColumns(10);
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Khu v\u1EF1c", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(148, 0, 211)));
@@ -77,102 +78,108 @@ public class BMISwing {
 		panel.setLayout(null);
 		
 		JRadioButton radA = new JRadioButton("Asian");
+		radA.setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		buttonGroup.add(radA);
 		radA.setBounds(39, 16, 58, 21);
 		panel.add(radA);
 		
 		JRadioButton radNonA = new JRadioButton("Non-Asian");
+		radNonA.setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		buttonGroup.add(radNonA);
 		radNonA.setBounds(125, 16, 90, 21);
 		panel.add(radNonA);
 		
-		JLabel lblWelc = new JLabel("CHÀO MỪNG CÁC BẠN ĐẾN VỚI CHƯƠNG TRÌNH TÍNH CHỈ SỐ BMI");
-		lblWelc.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWelc.setFont(new Font("Times New Roman", Font.BOLD, 15));
-		lblWelc.setForeground(new Color(148, 0, 211));
-		lblWelc.setBounds(10, 10, 642, 40);
-		frmChngTrnhTnh.getContentPane().add(lblWelc);
+		JLabel lblW = new JLabel("CHÀO MỪNG CÁC BẠN ĐẾN VỚI CHƯƠNG TRÌNH TÍNH CHỈ SỐ BMI");
+		lblW.setHorizontalAlignment(SwingConstants.CENTER);
+		lblW.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		lblW.setForeground(new Color(148, 0, 211));
+		lblW.setBounds(10, 10, 642, 40);
+		frmChngTrnhTnh.getContentPane().add(lblW);
 		
-		JLabel lblKq = new JLabel("");
-		lblKq.setToolTipText("");
-		lblKq.setBounds(45, 235, 341, 20);
-		frmChngTrnhTnh.getContentPane().add(lblKq);
+		JLabel lbl1 = new JLabel("");
+		lbl1.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		lbl1.setToolTipText("");
+		lbl1.setBounds(45, 235, 341, 20);
+		frmChngTrnhTnh.getContentPane().add(lbl1);
 		
-		JLabel lblAv = new JLabel("");
-		lblAv.setBounds(45, 257, 630, 20);
-		frmChngTrnhTnh.getContentPane().add(lblAv);
+		JLabel lbl2 = new JLabel("");
+		lbl2.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		lbl2.setBounds(45, 257, 630, 20);
+		frmChngTrnhTnh.getContentPane().add(lbl2);
 		
 		JButton btnTinhBMI = new JButton("Tính BMI");
+		btnTinhBMI.setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		btnTinhBMI.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String strChieuCao = txtCao.getText();
-				String strCanNang = txtNang.getText();
+				String strCC = txtCC.getText();
+				String strCN = txtCN.getText();
 				
-				double a = Double.parseDouble(strCanNang);
-				double b = Double.parseDouble(strChieuCao);
+				double a = Double.parseDouble(strCN);
+				double b = Double.parseDouble(strCC);
 				
-				double kq = a/(b*b);
+				double c = a/(b*b);
 				
-				kq = Math.ceil(kq*100)/100;
+				c = Math.ceil(c*100)/100;
 				
-				String av1 = "Bạn quá gầy! Bạn nên có một chế độ ăn uống giàu dinh dưỡng!";
-				String av2 = "Bạn có một thân hình cân đối! Cứ thế mà phát huy";
-				String av3 = "Bạn bị thừa cân. Bạn nên có chế độ ăn hợp lý và tập luyện thể dục thường xuyên!";
-				String av4 = "Bạn bị tiền béo phì. Bạn bên tìm gặp bác sĩ để được tư vấn về chế độ ăn kiêng cùng với các bài tập thể dục!";
-				String av5 = "Bạn bị béo phì. Bạn bên tìm gặp bác sĩ để được tư vấn về chế độ ăn kiêng cùng với các bài tập thể dục!";
-				String av6 = "Bạn bị béo phì cấp độ I. Bạn nên có một chế độ ăn uống lành mạnh hơn!";
-				String av7 = "Bạn bị béo phì độ II. Bạn bên tìm gặp bác sĩ để được tư vấn về chế độ ăn kiêng cùng với các bài tập thể dục!";
-				String av8 = "Bạn bị béo phì độ III. Bạn quá béo, bạn cần có chế độ ăn kiêng ngay và luôn!";
+				String lk1 = "Bạn quá gầy! Bạn nên có một chế độ ăn uống giàu dinh dưỡng!";
+				String lk2 = "Bạn có một thân hình cân đối! Cứ thế mà phát huy";
+				String lk3 = "Bạn bị thừa cân. Bạn nên có chế độ ăn hợp lý và tập luyện thể dục thường xuyên!";
+				String lk4 = "Bạn sắp bị béo phì. Bạn nên có chế độ ăn kiêng phù hợp cùng với các bài tập thể dục!";
+				String lk5 = "Bạn bị béo phì. Bạn cần có một chế độ ăn uống lành mạnh hơn!";
+				String lk6 = "Bạn bị béo phì cấp độ I. Bạn cần giảm khẩu phần ăn và tăng cường luyện tập thể dục hơn nữa!";
+				String lk7 = "Bạn bị béo phì độ II. Bạn bên tìm gặp bác sĩ để được tư vấn về chế độ ăn kiêng cùng với các bài tập thể dục!";
+				String lk8 = "Bạn bị béo phì độ III. Bạn quá béo, bạn cần có chế độ ăn kiêng ngay và luôn!";
 				
-				String temp;
-				temp= "Chỉ số BMI của bạn là "+ String.valueOf(kq);
+				String chisoBMI;
+				chisoBMI= "Chỉ số BMI của bạn là "+ String.valueOf(c) ;
 				
 				if (radA.isSelected() == true) {
-					if (kq<18.5) 
-						lblAv.setText(av1);
-					else 
-						if (kq>=18.5 && kq <25)
-							lblAv.setText(av2);
-						else
-							if (kq>=25 && kq <30)
-								lblAv.setText(av3);
-							else
-								if (kq>=29.9 && kq <=30)
-									lblAv.setText(av5);
-								else
-									if (kq>30 && kq <40)
-										lblAv.setText(av6);
-									else
-										if (kq>=40 && kq <50)
-											lblAv.setText(av7);
-										else
-											lblAv.setText(av8);
-				} else if (radNonA.isSelected() == true) {
-					if (kq<18.5) 
-						lblAv.setText(av1);
-					else 
-						if (kq>=18.5 && kq <23)
-							lblAv.setText(av2);
-						else
-							if (kq>=23 && kq <25)
-								lblAv.setText(av3);
-							else
-								if (kq>=25 && kq <29.9)
-									lblAv.setText(av4);
-								else
-									if (kq>=29.9 && kq <=30)
-										lblAv.setText(av5);
-									else
-										if (kq>30 && kq <40)
-											lblAv.setText(av6);
-										else
-											if (kq>=40 && kq <50)
-												lblAv.setText(av7);
-											else
-												lblAv.setText(av8);	
-						} else lblAv.setText("Bạn chưa chọn khu vực");
+				if (c<18.5) 
+					lbl2.setText(lk1);
+				else 
+				if (c>=18.5 && c <25)
+					lbl2.setText(lk2);
+				else
+				if (c>=25 && c <30)
+					lbl2.setText(lk3);
+				else
+				if (c>=29.9 && c <=30)
+					lbl2.setText(lk5);
+				else
+				if (c>30 && c <40)
+					lbl2.setText(lk6);
+				else
+				if (c>=40 && c <50)
+					lbl2.setText(lk7);
+				else
+					lbl2.setText(lk8);
+				} else 
+				if (radNonA.isSelected() == true) {
+				if (c<18.5) 
+					lbl2.setText(lk1);
+				else
+				if (c>=18.5 && c <23)
+					lbl2.setText(lk2);
+				else
+				if (c>=23 && c <25)
+					lbl2.setText(lk3);
+				else
+				if (c>=25 && c <29.9)
+					lbl2.setText(lk4);
+				else
+				if (c>=29.9 && c <=30)
+					lbl2.setText(lk5);
+				else
+				if (c>30 && c <40)
+					lbl2.setText(lk6);
+				else
+				if (c>=40 && c <50)
+					lbl2.setText(lk7);
+				else
+					lbl2.setText(lk8);	
+				} else lbl2.setText("Bạn chưa chọn khu vực");
 				
-				lblKq.setText(temp);
+				lbl1.setText(chisoBMI);
 			}
 		});
 		btnTinhBMI.setBounds(131, 199, 89, 25);
